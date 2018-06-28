72cc5d4a082f2f2c0c4b3c986ea4aa4357fcd738
Knight Fu <knight.w.fu@gmail.com>
2018-06-28 07:06:44 -0400

    adds coverage step

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,11 +1,43 @@
 #!/usr/bin/env bash
 
-for dir in `ls . | grep -v vendor`;
-do
-  if [[ -d ${dir} && ! "${dir}" =~ ^(scripts)$ ]]
-  then
-    go test ./${dir} -race -v -coverprofile cover.${dir}.out
-  fi
+if [ -z "${REPORTER}" ]
+then
+  export REPORTER=cc-test-reporter
+fi
 
-done
-go test . -race -v -coverprofile cover.out
+function unittest() {
+    # unit tests all the packages
+    for dir in `ls . | grep -v vendor`;
+    do
+        if [[ -d ${dir} && ! "${dir}" =~ ^(scripts)$ ]]
+        then
+            go test ./${dir} -vet -race -v -coverprofile cover.${dir}.out
+        fi
+
+    done
+
+    # unit test the main package
+    go test . -vet -race -v -coverprofile cover.out
+}
+
+
+function generate_coverage() {
+    echo "Using $REPORTER to generate JSON files"
+}
+
+
+# Execute the scripts
+case $1 in
+
+run)
+  unittest
+;;
+
+coverage)
+  generate_report
+;;
+
+*)
+  exit 1
+
+esac
