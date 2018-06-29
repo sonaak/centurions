be6bde4c9bc17f1c0f5cfbd5c9b3e341562bba53
Knight Fu <knight.w.fu@gmail.com>
2018-06-29 06:54:40 -0400

    adds tests for flow; adds step enums

--- core/flow_test.go
+++ core/flow_test.go
@@ -1,2 +1,41 @@
 package core
 
+import (
+	"testing"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+)
+
+func TestFlowNode(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "FlowNode")
+}
+
+var _ = Describe("FlowNode", func(){
+	It("should instantiate correctly with NewFlowNode", func(){
+		Expect(NewFlowNode(nil, nil, nil, nil)).NotTo(BeNil())
+	})
+})
+
+func TestFlowNodeStatus(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "FlowNodeStatus")
+}
+
+var _ = Describe("FlowNodeStatus", func(){
+	It("should instantiate correctly with NewFlowNodeStatus", func(){
+		Expect(NewFlowNodeStatus(FAILED, nil)).NotTo(BeNil())
+	})
+})
+
+func TestFlowState(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "FlowState")
+}
+
+var _ = Describe("FlowState", func(){
+	It("should instantiate correctly with NewFlowState", func(){
+		criteria := make([]FlowNodeStatus, 0, 0)
+		Expect(NewFlowState(criteria)).NotTo(BeNil())
+	})
+})
--- core/steps.go
+++ core/steps.go
@@ -3,6 +3,16 @@ package core
 
 type Status uint8
 
+const (
+	RUNNING = Status(0)
+	SKIPPED = Status(1)
+	PASSED = Status(2)
+	REDIRECT = Status(3)
+	FAILED = Status(4)
+	ERRORED = Status(5)
+	CANCELED = Status(6)
+	RETRYING = Status(7)
+)
 
 type Runnable interface {
 	Run() Status
