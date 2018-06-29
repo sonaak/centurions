9961271c6aa0e82b79a182a3aac45258eddf15df
Knight Fu <knight.w.fu@gmail.com>
2018-06-29 06:08:09 -0400

    adds a core suite for running gomega

---
+++ core/core_suite_test.go
@@ -0,0 +1,14 @@
+package core
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+
+func TestCore(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Core Suite")
+}
