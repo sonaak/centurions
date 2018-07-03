f3dd55f508e90ea2dc037935b57d5a55b74ad387
Knight Fu <knight.w.fu@gmail.com>
2018-07-03 05:52:37 -0400

    adds one test for Recorder Get

--- testing/steps_test.go
+++ testing/steps_test.go
@@ -1,1 +1,27 @@
 package testing
+
+import "testing"
+
+func TestRecorder_Get(t *testing.T) {
+	recorder := Recorder {
+		store: make(map[string]interface{}),
+	}
+
+	recorder.Set("a", uint(1))
+	val, err := recorder.Get("a")
+
+	if err != nil {
+		t.Errorf("expect no error when calling Get: %v", err)
+	}
+
+	switch v := val.(type) {
+	case uint:
+		if v != 1 {
+			t.Errorf("expect value to be 1 (uint); got %d instead", v)
+		}
+
+	default:
+		t.Errorf("expect value to be an integer")
+	}
+}
+
