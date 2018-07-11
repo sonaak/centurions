b38f6d69c512fafaee16f1f77e6ffff12955c18b
Knight Fu <knight.w.fu@gmail.com>
2018-07-11 06:55:02 -0400

    adds template; adds tests

--- files/generators.go
+++ files/generators.go
@@ -1,3 +1,34 @@
 package files
 
+import (
+	"text/template"
+	"bytes"
+)
 
+
+type Template struct {
+	tpl *template.Template
+}
+
+
+func ParseTemplate(base string) (*Template, error) {
+	tpl, err := template.New("base").Parse(base)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Template {
+		tpl: tpl,
+	}, nil
+}
+
+
+func (template *Template) Template(obj interface{}) (string, error) {
+	buffer := bytes.NewBuffer([]byte{})
+	err := template.tpl.Execute(buffer, obj)
+	if err != nil {
+		return "", err
+	}
+
+	return buffer.String(), nil
+}
--- files/generators_test.go
+++ files/generators_test.go
@@ -1,1 +1,41 @@
 package files
+
+import (
+	"testing"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+)
+
+func TestParseTemplate(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ParseTemplate")
+}
+
+var _ = Describe("ParseTemplate", func(){
+	It("should parse normal string correctly", func() {
+		template, err := ParseTemplate("hello, world")
+		Expect(err).To(BeNil())
+		Expect(template).ToNot(BeNil())
+		Expect(template.tpl).ToNot(BeNil())
+	})
+
+	It("should parse empty string correctly", func() {
+		template, err := ParseTemplate("")
+		Expect(err).To(BeNil())
+		Expect(template).ToNot(BeNil())
+		Expect(template.tpl).ToNot(BeNil())
+	})
+
+	It("should parse a valid template correctly", func() {
+		template, err := ParseTemplate("{{ .Hello }} - {{ .World }}")
+		Expect(err).To(BeNil())
+		Expect(template).ToNot(BeNil())
+		Expect(template.tpl).ToNot(BeNil())
+	})
+
+	It("should not parse an invalid, and should return an error", func() {
+		template, err := ParseTemplate("{{ .Hello } - {{ .World }}")
+		Expect(err).ToNot(BeNil())
+		Expect(template).To(BeNil())
+	})
+})
