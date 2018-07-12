9d22dd988c3c774494a52d7b8df0132bb523ce6a
Knight Fu <knight.w.fu@gmail.com>
2018-07-12 07:07:23 -0400

    adds more test to Template

--- files/generators_test.go
+++ files/generators_test.go
@@ -39,3 +39,57 @@ var _ = Describe("ParseTemplate", func(){
 		Expect(template).To(BeNil())
 	})
 })
+
+type testObject struct {
+	Integer int
+	Float64 float64
+	Message string
+
+	privateValue uint
+}
+
+func (obj *testObject) GetInteger() int {
+	return obj.Integer
+}
+
+
+var _ = Describe("Template", func() {
+	var testObj testObject
+
+	BeforeEach(func() {
+		testObj = testObject {
+			Integer: -20,
+			Float64: 0.321131,
+			Message: "hello, 世界",
+		}
+	})
+
+	It("should template correctly with public int attributes", func() {
+		template, _ := ParseTemplate("{{ .Integer }}")
+		Expect(template.Template(testObj)).To(Equal("-20"))
+	})
+
+	It("should template correctly with public float attributes", func() {
+		template, _ := ParseTemplate("{{ .Float64 }}")
+		Expect(template.Template(testObj)).To(Equal("0.321131"))
+	})
+
+	It("should template correctly with public string attributes", func() {
+		template, _ := ParseTemplate("{{ .Message }}")
+		Expect(template.Template(testObj)).To(Equal("hello, 世界"))
+	})
+
+	It("should not template correctly with private attributes", func() {
+		template, _ := ParseTemplate("{{ .privateValue }}")
+		str, err := template.Template(testObj)
+		Expect(err).ToNot(BeNil())
+		Expect(str).To(Equal(""))
+	})
+
+	It("should template correctly with public attributes", func() {
+		template, _ := ParseTemplate("{{ .GetInteger }}")
+		str, err := template.Template(&testObj)
+		Expect(err).To(BeNil())
+		Expect(str).To(Equal("-20"))
+	})
+})
